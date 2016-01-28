4f79dbb1da25eb1687755ff8b85330ef917df190
Wen <sunzibinfa@gmail.com>
2016-01-28 16:25:21 +0800

    完成 production 寄信設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,18 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.after_initialize do
-    Pay2go.integration_mode = :production
+    Pay2go.integration_mode = :development
   end
+  config.action_mailer.default_url_options = { host: 'https://peaceful-beach-93735.herokuapp.com' } # 你的 heroku app 網址
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:           587,
+    address:        "smtp.mailgun.org",
+    user_name:      ENV["mailgun_user"],
+    password:       ENV["mailgun_secret"],
+    domain:         "sandbox一堆亂碼.mailgun.org", # 你的 mailgun domain name
+
+    authentication: :plain,
+  }
 end
